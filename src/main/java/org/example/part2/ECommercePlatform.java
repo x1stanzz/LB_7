56e9cb1aeab656f197a9caee884cf5f1ce43f4cd
x1stanzz <vova28032003@gmail.com>
2023-11-26 17:00:24 +0200

    Create methods to display products by name, price and stock

--- src/main/java/org/example/part2/ECommercePlatform.java
+++ src/main/java/org/example/part2/ECommercePlatform.java
@@ -1,8 +1,8 @@
 package org.example.part2;
 
 import org.example.part1.*;
-import java.util.Map;
-import java.util.HashMap;
+
+import java.util.*;
 
 public class ECommercePlatform {
     private Map<Integer, User> users;
@@ -13,14 +13,24 @@ public class ECommercePlatform {
         this.products = new HashMap<>();
         this.orders = new HashMap<>();
     }
+    public Map<Integer, User> getUsers(){
+        return users;
+    }
+    public Map<Integer, Product> getProducts(){
+        return products;
+    }
+    public Map<Integer, Order> getOrders(){
+        return orders;
+    }
     public void addUser(User user){
         users.put(user.getId(), user);
     }
     public void addProduct(Product product){
         products.put(product.getId(), product);
     }
-    public void createOrder(User user, Map<Product, Integer> orderDetails){
+    public void createOrder(User user){
         int orderId = orders.size() + 1;
+        Map<Product, Integer> orderDetails = new HashMap<>(user.getCartCopy());
         Order order = new Order(orderId, user.getId());
         order.setOrderDetails(orderDetails);
         order.calculateTotalPrice();
@@ -53,7 +63,7 @@ public class ECommercePlatform {
     }
 
     public void updateProductStock(int productId, int newStock){
-        if(products.containsKey(productId)){
+        if(products.containsKey(productId) & newStock >= 0){
             Product product = products.get(productId);
             product.setStock(newStock);
         } else if (newStock < 0){
@@ -61,6 +71,45 @@ public class ECommercePlatform {
         }
         else {
             System.out.println("Product with ID " + productId + " not found!");
+        }
+    }
+    public void displayProductsByName(){
+        List<Product> sortedProducts = new ArrayList<>(getProducts().values());
+        Collections.sort(sortedProducts, new Product.NameComparator());
+        System.out.println("Products sorted by name: ");
+        for(Product product : sortedProducts){
+            System.out.println(product);
+        }
+    }
+    public void displayProductsByPrice(){
+        List<Product> sortedProducts = new ArrayList<>(getProducts().values());
+        Collections.sort(sortedProducts);
+
+        System.out.println("Products sorted by price: ");
+        for(Product product : sortedProducts){
+            System.out.println(product);
+        }
+    }
+    public void displayProductsByStock(){
+        List<Product> sortedProducts = new ArrayList<>(getProducts().values());
+        Collections.sort(sortedProducts, new Product.StockComparator());
+
+        System.out.println("Product sorted by stock:");
+        for(Product product : sortedProducts){
+            System.out.println(product);
+        }
+    }
+    public void displaySortedAvailableProducts(){
+        List<Product> availableProducts = new ArrayList<>();
+        for(Map.Entry<Integer, Product> entry : getProducts().entrySet()){
+            Product product = entry.getValue();
+            if(product.getStock() > 0){
+                availableProducts.add(product);
+            }
+        }
+        System.out.println("Available products: ");
+        for(Product product : availableProducts){
+            System.out.println(product);
         }
     }
 }
